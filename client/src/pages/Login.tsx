bd82069e1b9cfec0f9db40c8a1631c53dcc985dc
Ranjeet Kumar Jena <121615244+RANJEETJ06@users.noreply.github.com>
2024-01-09 22:47:25 +0530

    login failing problem solved

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,35 +1,29 @@
-// src/pages/Login.tsx
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [loginError, setLoginError] = useState(false);
-  const [userId,SetUserId] =useState(-1);
+  const [userId, SetUserId] = useState(-1);
   const navigate = useNavigate();
 
-  const handleLogin = async () => {
-    const isSuccessful = await performLogin(username, password);
-
-    if (isSuccessful) {
-      console.log(userId)
-      navigate(`/user/${userId}/Home/`);
-      console.log(userId);
-    } else {
-      setLoginError(true);
-    }
-  };
-  const performLogin = async (userName: string, password: string): Promise<boolean> => {
+  const performLogin = async (
+    userName: string,
+    password: string
+  ): Promise<boolean> => {
     try {
-      const response = await fetch(' http://localhost:8080/api/users/signing/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ userName, password }),
-      });
-  
+      const response = await fetch(
+        "http://localhost:8080/api/users/signing/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userName, password }),
+        }
+      );
+
       if (response.ok) {
         const result = await response.json();
         SetUserId(result.userId);
@@ -41,6 +35,20 @@ const Login: React.FC = () => {
     } catch (error) {
       console.error(`Error during sign-in: ${error}`);
       return false;
+    }
+  };
+
+  useEffect(() => {
+    if (userId !== -1) {
+      navigate(`/user/${userId}/Home/`);
+    }
+  }, [userId, navigate]);
+
+  const handleLogin = async () => {
+    const isSuccessful = await performLogin(username, password);
+
+    if (!isSuccessful) {
+      setLoginError(true);
     }
   };
 
@@ -65,7 +73,11 @@ const Login: React.FC = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </label>
-      {loginError && <p className="text-red-500 mb-2">Login failed. Check your credentials.</p>}
+      {loginError && (
+        <p className="text-red-500 mb-2">
+          Login failed. Check your credentials.
+        </p>
+      )}
       <button
         className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
         onClick={handleLogin}
